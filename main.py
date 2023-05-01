c2002123c6ea88c92ef030bbd40f77c4309ee1ca
Matt Hoffner <matthoffner@gmail.com>
2023-04-30 22:23:42 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-# How to use: python3 main.py --repo repo/path --model model/path > README.md :)
-# Saves json file to query again
-# Query index: python3 main.py --repo repo/path --query 'help'
-# Customize readme prompt: python3 main.py --repo repo/path 
 import os
 from subprocess import PIPE
 import argparse
 from langchain.llms import LlamaCpp
-from langchain.text_splitter import TokenTextSplitter
+from langchain.text_splitter import CharacterTextSplitter
 from adapter import HuggingFaceEmbeddings
 from llama_index import download_loader, GPTListIndex, LLMPredictor, PromptHelper, ServiceContext, Document, LangchainEmbedding
+from langchain.chains import ConversationalRetrievalChain
+from langchain.retrievers.llama_index import LlamaIndexRetriever
 from llama_index.node_parser import SimpleNodeParser
 from llama_index.data_structs import Node
 
@@ -30,29 +28,39 @@ parser.add_argument('--query', type=str, required=False)
 parser.add_argument('--prompt', type=str, required=False,
                     default="### Human: Summarize the file in README.md format")
 parser.add_argument('--readme', type=str, default="README.md")
+parser.add_argument('--qa', type=bool, required=False)
 args = parser.parse_args()
 
+llama = LlamaCpp(
+    model_path=args.model, 
+    n_ctx=4096, 
+    max_tokens=600, 
+    n_parts=-1, 
+    temperature=0.8, 
+    top_p=0.40,
+    last_n_tokens_size=400,
+    n_threads=8,
+    f16_kv=True,
+    use_mlock=True
+)
 llm_predictor = LLMPredictor(
-    llm=LlamaCpp(
-        model_path=args.model, 
-        n_ctx=2048, 
-        max_tokens=800, 
-        n_parts=-1, 
-        temperature=0.8, 
-        top_p=0.40,
-        last_n_tokens_size=1000,
-        n_threads=8,
-        f16_kv=True,
-        use_mlock=True
-    )
+    llm=llama
 )
 embeddings = HuggingFaceEmbeddings()
 embed_model = LangchainEmbedding(embeddings)
-node_parser = SimpleNodeParser(text_splitter=TokenTextSplitter())
+node_parser = SimpleNodeParser(text_splitter=CharacterTextSplitter(chunk_size=1000))
 service_context = ServiceContext.from_defaults(
     llm_predictor=llm_predictor, embed_model=embed_model, node_parser=node_parser,
     prompt_helper=prompt_helper
 )
+
+readme_wizard = [
+    "What does this project do?",
+    "How do I use this project?",
+    "Where can I find more information about related information in this proejct?"
+    "Who are the contributors to this project?",
+    "What is the motivation for this project?"
+] 
 
 if __name__ == "__main__":
     if args.read:
@@ -77,11 +85,19 @@ if __name__ == "__main__":
         project_path = path_components[0]
         output = index.query(args.prompt, service_context=service_context, mode="embedding")    
         readme = f"### {project_name}\n\n${output}"
+    if args.qa:
+        chat_history = []
+        retriever = LlamaIndexRetriever(index=index)
+        qa = ConversationalRetrievalChain.from_llm(llama, retriever=retriever)
+        for question in readme_wizard:  
+            result = qa({"question": question, "chat_history": chat_history})
+            chat_history.append((question, result['answer']))
+            print(f"## {question} \n")
+            print(f"{result['answer']} \n")
+            output = f"{output}\n## {question}\n {result['answer']}\n"
     
-    readme2 = readme.replace("$### Assistant: ", "")
+    readme2 = readme.replace("### Assistant: ", "")
     print(readme2)
     with open(f"{project_path}/{args.readme}", 'a') as file:
         file.write(readme2)
         file.close()
-        
-    
